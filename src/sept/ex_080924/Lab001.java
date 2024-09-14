94f35be6edbf72b3be5faa20041f93b1a983b598
Liki06 <likithliki73@gmail.com>
2024-09-14 17:19:22 +0530

    First Assignment added

--- src/sept/ex_080924/Lab001.java
+++ src/sept/ex_080924/Lab001.java
@@ -3,6 +3,12 @@ package sept.ex_080924;
 public class Lab001 {
     public static void main(String[] args)
     {
+System.out.println("Likith");
+
+/*   likith Needs to be good automation tester
+
+*/
+
 
     }
 }

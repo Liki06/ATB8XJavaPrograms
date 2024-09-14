5d25077f4fb8c1e7bd5e84e1c3ca48def6935c78
Liki06 <likithliki73@gmail.com>
2024-09-14 22:09:38 +0530

    Second Assignment is completed

--- src/sept/Assignment_1/Assignment_2.java
+++ src/sept/Assignment_1/Assignment_2.java
@@ -1,6 +1,19 @@
+//Modulus operator=
 package sept.Assignment_1;
-
-public class Assignment_2 {
+public class Assignment_2
+{
+    public static void main(String[] args)
+    {
+        double a=50.0;
+        double b=50.0;
+        double sum=a+b;
+        System.out.println("*****Sample Calculator*******");
+        System.out.println("Addition Calculator for values:"+ (a+b));
+        System.out.println("Subtraction Calculator for values:"+ (a-b));
+        System.out.println("Multiplication Calculator for values:"+ (a*b));
+        System.out.println("Division Calculator for values:"+ (a/b));
+        System.out.println("Modulus Calculator for values:"+ (a%b));
+    }
 
 
 }
---
+++ src/sept/ex130924/binary_hex.java
@@ -0,0 +1,9 @@
+package sept.ex130924;
+public class binary_hex {
+    public static void main(String[] args) {
+        int binary_num=0b1010;
+        int octal=0xFACE;
+        System.out.println(binary_num);
+        System.out.println(octal);
+    }
+}

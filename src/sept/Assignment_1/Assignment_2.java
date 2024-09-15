6da73253584557300b7bb73fcbc5faadb71afdb9
Liki06 <likithliki73@gmail.com>
2024-09-15 13:21:01 +0530

    Updated the Assignment for second Assignment_2.java

--- src/sept/Assignment_1/Assignment_2.java
+++ src/sept/Assignment_1/Assignment_2.java
@@ -7,12 +7,32 @@ public class Assignment_2
         double a=50.0;
         double b=50.0;
         double sum=a+b;
-        System.out.println("*****Sample Calculator*******");
-        System.out.println("Addition Calculator for values:"+ (a+b));
-        System.out.println("Subtraction Calculator for values:"+ (a-b));
-        System.out.println("Multiplication Calculator for values:"+ (a*b));
-        System.out.println("Division Calculator for values:"+ (a/b));
-        System.out.println("Modulus Calculator for values:"+ (a%b));
+//        System.out.println("*****Sample Calculator*******");
+//        System.out.println("Addition Calculator for values:"+ (a+b));
+//        System.out.println("Subtraction Calculator for values:"+ (a-b));
+//        System.out.println("Multiplication Calculator for values:"+ (a*b));
+//        System.out.println("Division Calculator for values:"+ (a/b));
+//        System.out.println("Modulus Calculator for values:"+ (a%b));
+      //we need to use %f for double
+        System.out.printf("%f + %f = %f",a,b,(a+b));
+        System.out.println();
+        System.out.printf("%f - %f = %f",a,b,(a-b));
+        System.out.println();
+        System.out.printf("%f * %f = %f",a,b,(a*b));
+        System.out.println();
+        System.out.printf("%f / %f = %f",a,b,(a/b));
+        System.out.println();
+        System.out.printf("%f mod %f = %f",a,b,(a % b));
+        System.out.println();
+
+
+
+
+
+
+
+
+
     }
 
 

38ad2d20692a51095f0cdb3f7b7179826cc36dd5
Liki06 <likithliki73@gmail.com>
2024-10-02 20:24:53 +0530

    Updated the Assignment for second Assignment_5.java

--- src/sept/Assignment/Assignment_13.java
+++ src/sept/Assignment/Assignment_13.java
@@ -1,19 +1,31 @@
 package sept.Assignment;
 
 public class Assignment_13 {
-    public static void main(String[] args)
-    {
-      /*
+    public static void main(String[] args) {
+        int n = 100;    /*
 
       Prime Number from 1 to 100. (for loop)
        */
         //The number which is divisible by 1 and itself is called prime number
-        for(int i=1;i<=100;i++)
-        {
-           if(i%1==0 && i%i==0)
-           {
-               System.out.println("It is prime number" + i);
-           }
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = i; j >= 1; j--) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+                if (count == 2)
+                {
+                    System.out.println("It is prime number:" + i);
+                }
+
+            }
+
         }
     }
-}
+
+
+
+
+
